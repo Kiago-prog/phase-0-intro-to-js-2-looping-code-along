8fd42c0dc5bb33bba376b5686f07e795b3d12bde
Kiago-prog <roy.kiago@student.moringaschool.com>
2025-03-03 12:10:44 +0300

    Count

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const Number=10
+function writeCards() {
+    return[
+    "Thank you, Guadalupe, for the wonderful surprise gift!",
+    "Thank you, Ollie, for the wonderful surprise gift!",
+     "Thank you, Aki, for the wonderful surprise gift!",
+    ];
+}
+
+function countDown(Number) {
+    while (Number >= 0) {
+        console.log(Number);
+        Number--;
+    }
+}
